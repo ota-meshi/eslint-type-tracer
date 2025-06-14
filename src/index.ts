a3e7c293444a1d5f9ea862f499ce3d486c79be92
Yosuke Ota <otameshiyo23@gmail.com>
2025-06-14 21:49:37 +0900

    fix: redesign API (#3)
    * fix: redesign API
    
    * add changeset

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export { buildTypeChecker } from "./type-tracer/type-tracer";
+export {
+  buildTypeChecker,
+  type TypeCheckerOptions,
+} from "./type-tracer/type-tracer";
 export type { TypeName } from "./type-tracer/types";
+export { type TypeChecker } from "./type-tracer/utils";
