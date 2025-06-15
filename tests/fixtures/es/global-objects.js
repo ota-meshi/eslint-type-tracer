8c10486fdf79e5ebd4c093920f75077fcb28f7bd
Yosuke Ota <otameshiyo23@gmail.com>
2025-06-15 15:53:33 +0900

    test: update tests (#7)

---
+++ tests/fixtures/es/global-objects.js
@@ -0,0 +1,79 @@
+target(String(), String``);
+
+target(Number(), Number``);
+
+target(BigInt(), BigInt``);
+
+target(Boolean(), Boolean``);
+
+target(Symbol(), Symbol``);
+
+target(Object(), Object``);
+
+target([], Array``);
+
+target(Function(), Function``);
+
+target(RegExp(), RegExp``);
+
+target(Date(), Date``);
+
+target(new String());
+
+target(new Number());
+
+target(new BigInt());
+
+target(new Boolean());
+
+target(new Symbol());
+
+target(new Object());
+
+target(new Array());
+
+target(new Function());
+
+target(new RegExp());
+
+target(new Date());
+
+target(new Promise());
+
+target(new Intl.Collator());
+
+target(new Intl.DateTimeFormat());
+
+target(new Intl.ListFormat());
+
+target(new Intl.NumberFormat());
+
+target(new Intl.PluralRules());
+
+target(new Intl.RelativeTimeFormat());
+
+target(new Intl.Segmenter());
+
+target(new Int8Array());
+
+target(new Uint8Array());
+
+target(new Uint8ClampedArray());
+
+target(new Int16Array());
+
+target(new Uint16Array());
+
+target(new Int32Array());
+
+target(new Uint32Array());
+
+target(new Float16Array());
+
+target(new Float32Array());
+
+target(new Float64Array());
+
+target(new BigInt64Array());
+
+target(new BigUint64Array());
