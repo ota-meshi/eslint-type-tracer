c0ab52daa88a647a062b5bff199cd11ce370af2b
yosuke ota <otameshiyo23@gmail.com>
2025-06-15 10:21:34 +0900

    add test

--- tests/src/type-tracer/type-tracer-for-es.ts
+++ tests/src/type-tracer/type-tracer-for-es.ts
@@ -1,527 +1,94 @@
 import { deepStrictEqual } from "assert";
 import { buildTypeTracerForES } from "../../../src/type-tracer/type-tracer-for-es";
+import { expect } from "@ota-meshi/test-snapshot";
 import type { TypeName } from "../../../src/type-tracer/types";
 import type { Rule } from "eslint";
 import { Linter } from "eslint";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
 
-// -----------------------------------------------------------------------------
-// Tests
-// -----------------------------------------------------------------------------
+const FIXTURES_ROOT = path.resolve(
+  fileURLToPath(import.meta.url),
+  "../../../fixtures/es",
+);
+
+function* extractFixtures(dir = FIXTURES_ROOT): Generator<{
+  name: string;
+  code: string;
+  filename: string;
+  only?: boolean;
+}> {
+  for (const dirent of fs.readdirSync(dir, { withFileTypes: true })) {
+    const filePath = path.join(dir, dirent.name);
+    if (dirent.isFile()) {
+      if (dirent.name.endsWith(".js")) {
+        yield {
+          name: path.relative(FIXTURES_ROOT, filePath),
+          code: fs.readFileSync(filePath, "utf8"),
+          filename: filePath,
+        };
+      }
+    } else if (dirent.isDirectory()) {
+      yield* extractFixtures(filePath);
+    }
+  }
+}
 
 describe("type-tracer-for-es", () => {
   describe("buildTypeTracerForES", () => {
-    for (const { code, result, only } of [
-      {
-        code: "target('foo');",
-        result: ["String"],
-      },
-      {
-        code: "target(42);",
-        result: ["Number"],
-      },
-      {
-        code: "target(true);",
-        result: ["Boolean"],
-      },
-      {
-        code: "target(/foo/);",
-        result: ["RegExp"],
-      },
-      {
-        code: "target(null);",
-        result: ["null"],
-      },
-      {
-        code: "target(42n);",
-        result: ["BigInt"],
-      },
-      {
-        code: "target([]);",
-        result: ["Array"],
-      },
-      {
-        code: "target({});",
-        result: ["Object"],
-      },
-      {
-        code: "target(function(){});",
-        result: ["Function"],
-      },
-      {
-        code: "target(()=>{});",
-        result: ["Function"],
-      },
-      {
-        code: "target(a);",
-        result: [null],
-      },
-      {
-        code: "target(NaN);",
-        result: ["Number"],
-      },
-      {
-        code: "target(Infinity);",
-        result: ["Number"],
-      },
-      {
-        code: "target(undefined);",
-        result: ["undefined"],
-      },
-      {
-        code: `
-                const a = 'i'
-                target(a);`,
-        result: ["String"],
-      },
-      {
-        code: `
-                function f(){}
-                target(f);`,
-        result: ["Function"],
-      },
-      {
-        code: `
-                const a = b
-                target(a);`,
-        result: [null],
-      },
-      {
-        code: `
-                const a = a
-                target(a);`,
-        result: [null],
-      },
-      {
-        code: `
-                var a = 'foo'
-                var a = 42
-                target(a);`,
-        result: [null],
-      },
-      {
-        code: `
-                var a = 'foo'
-                a = 42
-                var b = 'bar'
-                target(a, b);`,
-        result: [null, "String"],
-      },
-      {
-        code: "target(`foo`);",
-        result: ["String"],
-      },
-      {
-        code: "target(`foo${a}`);",
-        result: ["String"],
-      },
-      {
-        code: `
-                const a = 'i'
-                target('foo' + 'bar');
-                target(a + 'foo');
-                target('foo' + a + 'bar');
-            `,
-        result: ["String", "String", "String"],
-      },
-      {
-        code: `
-                const a = 'i'
-                target(b + 'bar');
-                target('foo' + a + b);
-                `,
-        result: ["String", "String"],
-      },
-      {
-        code: `
-                const a = 42
-                target(
-                    a + 'bar',
-                    foo + 'bar',
-                    'bar' + foo,
-                    42 + a,
-                    {} + 42,
-                    42 + null,
-                    42 + {},
-                    42n + foo,
-                    42 + foo,
-                );
-                `,
-        result: [
-          "String",
-          "String",
-          "String",
-          "Number",
-          "Number",
-          "Number",
-          "String",
-          "BigInt",
-          null,
-        ],
-      },
-      {
-        code: `
-                let b = 42
-                target(
-                    b += 'bar',
-                );
-                `,
-        result: ["String"],
-      },
-      {
-        code: `
-                let b = 42
-                target(
-                    b = 'bar',
-                );
-                `,
-        result: ["String"],
-      },
-      ...[
-        "==",
-        "!=",
-        "===",
-        "!==",
-        "<",
-        "<=",
-        ">",
-        ">=",
-        "in",
-        "instanceof",
-      ].map((op) => ({
-        code: `
-                target(
-                    foo ${op} bar,
-                );
-                `,
-        result: ["Boolean"],
-      })),
-      ...["-", "*", "/", "%", "^", "**", "&", "|"].map((op) => ({
-        code: `
-                target(
-                    foo ${op} bar,
-                    42 ${op} bar,
-                    foo ${op} 42,
-                    42n ${op} bar,
-                    foo ${op} 42n,
-                );
-                `,
-        result: [null, "Number", "Number", "BigInt", "BigInt"],
-      })),
-      ...["-", "*", "/", "%", "^", "**", "&", "|"].map((op) => ({
-        code: `
-                target(
-                    foo ${op}= bar,
-                    foo ${op}= 42,
-                    foo ${op}= 42n,
-                );
-                `,
-        result: [null, "Number", "BigInt"],
-      })),
-      ...["&&", "||", "??"].map((op) => ({
-        code: `
-                    const a = 1
-                    const b = 1
-                    const c = 1n
-                    const d = 1n
-                    target(
-                        a ${op} b,
-                        c ${op} d,
-                        foo ${op} bar,
-                        42 ${op} bar,
-                        foo ${op} 42,
-                        42n ${op} bar,
-                        foo ${op} 42n,
-                    );
-                    `,
-        result: ["Number", "BigInt", null, null, null, null, null],
-      })),
-      ...["&&", "||", "??"].map((op) => ({
-        code: `
-                const a = 1
-                const b = 1
-                const c = 1n
-                const d = 1n
-                target(
-                    a ${op}= b,
-                    c ${op}= d,
-                    foo ${op}= bar,
-                    foo ${op}= 42,
-                    foo ${op}= 42n,
-                );
-                `,
-        result: ["Number", "BigInt", null, null, null],
-      })),
-      ...["<<", ">>", ">>>"].map((op) => ({
-        code: `
-                target(
-                    foo ${op} bar,
-                );
-                `,
-        result: ["Number"],
-      })),
-      ...["<<", ">>", ">>>"].map((op) => ({
-        code: `
-                target(
-                    foo ${op}= bar,
-                );
-                `,
-        result: ["Number"],
-      })),
-      {
-        code: `
-                target(
-                    !foo,
-                    delete foo.bar
-                );
-                `,
-        result: ["Boolean", "Boolean"],
-      },
-      {
-        code: `
-                target(
-                    +bar,
-                );
-                `,
-        result: ["Number"],
-      },
-      ...["-", "~"].map((op) => ({
-        code: `
-                target(
-                    ${op}foo,
-                    ${op}42,
-                    ${op}42n,
-                );
-                `,
-        result: [null, "Number", "BigInt"],
-      })),
-      {
-        code: `
-                target(
-                    typeof bar,
-                    void bar,
-                );
-                `,
-        result: ["String", "undefined"],
-      },
-      {
-        code: `
-                target(
-                    (foo,'bar'),
-                );
-                `,
-        result: ["String"],
-      },
-      {
-        code: `
-                target(
-                    class Foo {},
-                );
-                `,
-        result: ["Function"],
-      },
-      ...[
-        "String",
-        "Number",
-        "BigInt",
-        "Boolean",
-        "Symbol",
-        "Object",
-        "Array",
-        "Function",
-        "RegExp",
-        "Date",
-      ].map((t) => ({
-        code: `
-                target(
-                    ${t}(),
-                    ${t}\`\`,
-                );
-                `,
-        result: [t, t],
-      })),
-      ...[
-        "String",
-        "Number",
-        "BigInt",
-        "Boolean",
-        "Symbol",
-        "Object",
-        "Array",
-        "Function",
-        "RegExp",
-        "Date",
-        "Promise",
-        "Intl.Collator",
-        "Intl.DateTimeFormat",
-        "Intl.ListFormat",
-        "Intl.NumberFormat",
-        "Intl.PluralRules",
-        "Intl.RelativeTimeFormat",
-        "Intl.Segmenter",
-        "Int8Array",
-        "Uint8Array",
-        "Uint8ClampedArray",
-        "Int16Array",
-        "Uint16Array",
-        "Int32Array",
-        "Uint32Array",
-        "Float16Array",
-        "Float32Array",
-        "Float64Array",
-        "BigInt64Array",
-        "BigUint64Array",
-      ].map((t) => ({
-        code: `
-                target(
-                    new ${t}(),
-                );
-                `,
-        result: [t],
-      })),
-      {
-        code: `
-                target(
-                    a ? 'a' : 'b',
-                    a ? 'a' : 42,
-                );
-                `,
-        result: ["String", null],
-      },
-      {
-        code: `
-                target(
-                    a++,
-                    --a,
-                );
-                `,
-        result: ["Number", "Number"],
-      },
-      {
-        code: `
-                target("".length);
-                `,
-        result: ["Number"],
-      },
-      {
-        code: `
-                target(
-                    String.fromCharCode(42),
-                    String.fromCodePoint(42),
-                    String.raw\`\`,
-                );
-                `,
-        result: ["String", "String", "String"],
-      },
-      {
-        code: `
-                const { promise, resolve, reject } = Promise.withResolvers()
-                target(promise, resolve, reject);
-                `,
-        result: ["Promise", "Function", "Function"],
-      },
-      {
-        code: `
-                const r = /foo/.exec('foo')[0]
-                target(r);
-                `,
-        result: ["String"],
-      },
-      {
-        code: `
-                let array = []
-                array = []
-                target(array);
-                `,
-        result: ["Array"],
-      },
-      {
-        code: `
-                let array = []
-                array = ''
-                target(array);
-                `,
-        result: [null],
-      },
-      {
-        code: `
-                let {EPSILON:a} = Number;
-                a = 42;
-                ({MAX_SAFE_INTEGER:a} = Number);
-                target(a);
-                `,
-        result: ["Number"],
-      },
-      {
-        code: `
-                let a = Number.isFinite;
-                a =  Number.isNaN;
-                target(a(foo));
-                `,
-        result: ["Boolean"],
-      },
-      {
-        code: `
-                let a = Number.isFinite;
-                a =  Number;
-                target(a(foo));
-                `,
-        result: [null],
-      },
-      {
-        code: `
-                let {a=Number.isFinite} = {a:Number.isInteger};
-                a =  Number.isNaN;
-                target(a(foo));
-                `,
-        result: ["Boolean"],
-      },
-      {
-        code: `
-                target(Math.max(1,2));
-                target(Math.fround(5));
-                target(Math.f16round(5));
-                `,
-        result: ["Number", "Number", "Number"],
-      },
-    ] as { code: string; result: (TypeName | null)[]; only?: boolean }[]) {
-      (only ? it.only : it)(code, () => {
-        deepStrictEqual(getResultOfBuildTypeTracerForES(code), result);
+    for (const { name, code, filename, only } of extractFixtures()) {
+      describe(name, () => {
+        (only ? it.only : it)(code, () => {
+          expect(
+            getResultOfBuildTypeTracerForTS(code, filename),
+          ).toMatchSnapshot();
+        });
       });
     }
   });
 });
 
-function getResultOfBuildTypeTracerForES(code: string) {
+function getResultOfBuildTypeTracerForTS(code: string, filename: string) {
   const linter = new Linter({ configType: "flat" });
 
-  const result: (TypeName | null)[] = [];
-  const linterResult = linter.verify(code, {
-    plugins: {
-      test: {
-        rules: {
-          "test-rule": {
-            create(context: Rule.RuleContext) {
-              const getType = buildTypeTracerForES(context.sourceCode);
-              return {
-                "CallExpression[callee.name = target]"(node) {
-                  result.push(...node.arguments.map(getType));
-                },
-              };
+  const result: {
+    expr: string;
+    types: (TypeName | null)[];
+  }[] = [];
+  const linterResult = linter.verify(
+    code,
+    {
+      files: ["**/*.*"],
+      plugins: {
+        test: {
+          rules: {
+            "test-rule": {
+              create(context: Rule.RuleContext) {
+                const getType = buildTypeTracerForES(context.sourceCode);
+                return {
+                  "CallExpression[callee.name = target]"(node) {
+                    result.push({
+                      expr: context.sourceCode.getText(node),
+                      types: node.arguments.map(getType),
+                    });
+                  },
+                };
+              },
             },
           },
         },
       },
-    },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        Float16Array: "readonly",
-        Intl: "readonly",
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
+      rules: { "test/test-rule": "warn" },
     },
-    rules: { "test/test-rule": "warn" },
-  });
+    {
+      filename,
+    },
+  );
   deepStrictEqual(linterResult, []);
   return result;
 }
